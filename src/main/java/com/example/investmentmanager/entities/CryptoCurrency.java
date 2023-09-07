b9894a78a4adaf72b0893012715f1012b084066e
Janek1010 <janekdomozych99@gmail.com>
2023-09-07 10:09:59 +0200

    sql properties

--- src/main/java/com/example/investmentmanager/entities/CryptoCurrency.java
+++ src/main/java/com/example/investmentmanager/entities/CryptoCurrency.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,6 +23,7 @@ public class CryptoCurrency {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36,columnDefinition = "varchar(36)", updatable = false,nullable = false)
     private UUID id;
 
